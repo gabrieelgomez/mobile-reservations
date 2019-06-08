2de72d97714f62d784e40649d3d1761ba16b7e13
gabrielgomez <gagg1707@gmail.com>
2019-06-08 17:42:01 -0400

    add view reservation

--- src/components/transfers/reservations/Reservation.js
+++ src/components/transfers/reservations/Reservation.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { ScrollView, StyleSheet, View, Text, RefreshControl } from 'react-native';
 import { Button, Divider } from 'react-native-elements'
+import { TextField } from 'react-native-material-textfield';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Reservation extends Component {
 
@@ -37,7 +39,6 @@ export default class Reservation extends Component {
             <Text style={styles.subtitle}>{this.state.dataVehicle.attributes.title['es']}</Text>
           </View>
 
-
           <View style={styles.boxTitles}>
             <Text style={styles.title}>Datos de Reserva</Text>
             <Divider style={[styles.dividerStyles, { width: 180 }]} />
@@ -46,9 +47,230 @@ export default class Reservation extends Component {
                 <View>
                   <Text style={styles.subtitle}> <Text style={styles.strong}>Desde:  </Text>{this.state.dataReservation.origin_locality}</Text>
                   <Text style={styles.subtitle}> <Text style={styles.strong}>Hasta:  </Text>{this.state.dataReservation.arrival_locality}</Text>
+
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Cantidad de Equipaje'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='Tipo de Traslado'
+                      disabled
+                      value={this.state.dataReservation.round_trip.toString()}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Hora Origen'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='Hora Destino'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_kids}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
                 </View>
               )
             }
+          </View>
+
+
+          <View style={styles.boxTitles}>
+            <Text style={styles.title}>Datos de Viajeros</Text>
+            <Divider style={[styles.dividerStyles, { width: 180 }]} />
+            {
+              this.state.dataReservation && (
+                <View>
+
+                  <Text style={styles.travellerTitle}> Viajero #1 </Text>
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Identificación'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='Nombre y Apellido'
+                      // value={this.props.dataForm.quantity_kids}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
+                  <Text style={styles.travellerTitle}> Viajero #2 </Text>
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Identificación'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='Nombre y Apellido'
+                      // value={this.props.dataForm.quantity_kids}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
+                </View>
+              )
+            }
+          </View>
+
+
+          <View style={styles.boxTitles}>
+            <Text style={styles.title}>Datos de Facturación</Text>
+            <Divider style={[styles.dividerBilling, { width: 180 }]} />
+            {
+              this.state.dataReservation && (
+                <View>
+
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Identificación'
+                      keyboardType = 'numeric'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='Nombre y Apellido'
+                      // value={this.props.dataForm.quantity_kids}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      label='Teléfono'
+                      keyboardType = 'phone-pad'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputRight]}
+                      tintColor='#9dc107'
+                      label='E-mail'
+                      keyboardType = 'email-address'
+                      // value={this.props.dataForm.quantity_kids}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+
+                  </View>
+
+                  <View style={[styles.boxInputFlexTwo]}>
+                    <TextField
+                      labelTextStyle={styles.fontFamily}
+                      containerStyle={[styles.containerInput, styles.inputLeft]}
+                      tintColor='#9dc107'
+                      multiline={true}
+                      label='Dirección de Factura'
+                      // value={this.props.dataForm.quantity_adults}
+                      onChangeText={text =>
+                        console.log(text)
+                      }
+                    />
+                  </View>
+
+                </View>
+              )
+            }
+          </View>
+
+          <View style={[styles.boxBottonSend]}>
+            <Button
+              title='CONFIRMAR'
+              type='outline'
+              // icon={
+              //   <Icon
+              //     name='search'
+              //     size={15}
+              //     color='white'
+              //     iconContainerStyle={styles.iconContainerStyle}
+              //   />
+              // }
+              raised={true}
+              buttonStyle={styles.buttonSend}
+              titleStyle={styles.buttonTitleStyle}
+              // onPress={this.handlePress}
+            />
           </View>
 
         </View>
@@ -81,6 +303,11 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginBottom: 10
   },
+  travellerTitle:{
+    fontFamily: 'Poppins-Medium',
+    fontSize: 12,
+    marginBottom: -10
+  },
   strong:{
     fontFamily: 'Poppins-Bold',
     fontSize: 12,
@@ -90,21 +317,60 @@ const styles = StyleSheet.create({
     backgroundColor: '#e1e8ee',
     marginBottom: 15
   },
+  dividerBilling: {
+    height: 3,
+    backgroundColor: '#e1e8ee',
+  },
   imageStyle: {
     height: 200,
     borderBottomColor: '#d6d7da',
     borderBottomWidth: 1
   },
-  buttonReservation: {
-    backgroundColor: '#9dc107',
-    borderColor: '#9dc107',
+  boxBottonSend: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "stretch",
+    marginTop: 20,
+    marginBottom: 30
+  },
+  buttonSend: {
+    backgroundColor: "#9dc107",
+    borderColor: "#9dc107",
     borderRadius: 5,
     padding: 10,
-    width: 340
+    width: 360
   },
   buttonTitleStyle: {
     fontFamily: 'Poppins-Regular',
     color: '#fff',
-    marginLeft: 20,
+    // marginLeft: 20,
   },
+  iconContainer:{
+    paddingRight: 20
+  },
+
+  boxInputFlexTwo: {
+    paddingTop: 0,
+    paddingBottom: 25,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignSelf: 'stretch',
+  },
+  containerInput:{
+    flex: 1,
+  },
+  inputLeft:{
+    paddingRight: 20
+  },
+  inputRight:{
+    paddingLeft: 20
+  },
+  fontFamily:{
+    fontFamily: 'Poppins-Light',
+  },
+
 });
